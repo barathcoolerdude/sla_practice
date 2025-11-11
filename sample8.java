07b7898a0aa02485b1d119a270fb264c2410893a
coolerdude <barathelango3@gmail.com>
2025-11-11 15:44:28 +0530

    prakadeesh code

--- sample8.java
+++ sample8.java
@@ -1,13 +1,19 @@
 import java.util.*;
-public class sample8{
-    public static void main(String []args){
+class sample8{
+    public static void main(String[]args){
         Scanner s=new Scanner(System.in);
-        System.out.println("enter a number: ");
-        int number = s.nextInt();
-        s.nextLine();
-        for(int i =1; i<11; i++) {
-            System.out.println(number+" x "+i+" ="+(number*i));
-        }
-        s.close();
+        int[] num= {10,20,30,20,10,10};
+for(int i=0;i<num.length;i++){
+			int count=1;
+			if(num[i]==0) continue;
+			for(int j=i+1;j<num.length;j++){
+				if(num[i]==num[j]){
+					count+=1;
+					num[j]=0;
+				}
+			}
+			System.out.println(num[i]+" comes "+count+" times");	
+		}
+    s.close();
     }
 }
