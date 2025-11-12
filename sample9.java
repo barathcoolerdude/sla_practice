888b409eb67d472504960e47d056e5abf475c29a
coolerdude <barathelango3@gmail.com>
2025-11-12 14:42:43 +0530

    print random things with nested loop

--- sample9.java
+++ sample9.java
@@ -7,5 +7,36 @@ public class sample9{
             }
             System.out.print("\n");
         }
+        
+
+        for(int i = 1;i<=3;i++){
+            for(int j = 1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+
+            for(int i=0; i<=3;i++){
+                for(int j=1;j<=i;j++){
+                    System.out.print(j+" ");
+                }
+                System.out.print("\n");
+            }
+
+            for(int i=0; i<=3;i++){
+                for(int j=1;j<=i;j++){
+                    System.out.print(i+" ");
+                }
+                System.out.print("\n");
+            }
+
+            int count = 1;
+            for(int i=0; i<=3;i++){
+                for(int j=1;j<=i;j++){
+                    System.out.print(count+" ");
+                    count++;
+                }
+                System.out.print("\n");
+            }
     }
 }
