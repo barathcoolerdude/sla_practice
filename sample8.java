a61bde3a522c771027c96490c5045a31b1a4ad1f
coolerdude <barathelango3@gmail.com>
2025-11-12 14:32:11 +0530

    print 9 stars in 3 lines

--- sample8.java
+++ sample8.java
@@ -1,19 +1,31 @@
+import java.io.*;
 import java.util.*;
-class sample8{
-    public static void main(String[]args){
-        Scanner s=new Scanner(System.in);
-        int[] num= {10,20,30,20,10,10};
-for(int i=0;i<num.length;i++){
-			int count=1;
-			if(num[i]==0) continue;
-			for(int j=i+1;j<num.length;j++){
-				if(num[i]==num[j]){
-					count+=1;
-					num[j]=0;
-				}
-			}
-			System.out.println(num[i]+" comes "+count+" times");	
-		}
-    s.close();
-    }
+
+class sample8
+{
+public static void main(String []ars)
+{
+System.out.println("enter the year");
+Scanner s=new Scanner (System.in);
+
+int years= s.nextInt();
+boolean isLeapYear;
+
+ if (years % 100 == 0 || years % 400 == 0) {
+            isLeapYear = false;
+        } else if (years % 4 == 0) {
+            isLeapYear = true;
+        } else {
+            isLeapYear = false;
+        }
+if (isLeapYear)
+{
+System.out.println(years + "is a leap year");
+}
+else
+{
+System.out.println(years + "is a not leap year");
+}
+s.close();
+}
 }
---
+++ sample9.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class sample9{
+    public static void main(String []args){
+        for(int i = 0;i<3;i++){
+            for(int j = 0;j< 3;j++){
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
