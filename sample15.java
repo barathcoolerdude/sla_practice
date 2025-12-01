599b2e48f53bd5cd9b0eb2e2b068b52550536749
coolerdude <barathelango3@gmail.com>
2025-12-01 14:46:01 +0530

    file reading

---
+++ sample15.java
@@ -0,0 +1,25 @@
+import java.io.*;
+class sample15{
+    public static void main(String []args){
+        System.out.println("some");
+        try{
+            File file=new File("//home//coolerdude//java//sla_pratice//sample15.txt");
+            FileWriter fw=new FileWriter(file);
+            String word='o';
+            fw.write(word);
+            fw.close();
+            
+            FileReader fr=new FileReader(file);
+            char result=(char)fr.read();
+
+            System.out.println("result: "+result);
+            fr.close();
+
+        }catch(IOException e){
+            System.out.println(e);
+
+        }
+    }
+}
+
+
