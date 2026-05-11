d5344bdc3dcccc7cd891567243a788eca2c006c9
coolerdude <barathelango3@gmail.com>
2026-05-11 13:17:42 +0530

    java revice

---
+++ revice.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class revice{
+    public static void main(String []args){
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i=1;i <= 5;i++){
+            System.out.println(i);
+        }
+        scanner.close();
+    }
+}
